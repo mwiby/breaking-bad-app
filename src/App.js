9a1c5a944a0eccfc30366b7c9967981dbde63e9c
mwiby <markuswb77@gmail.com>
2020-11-22 15:38:02 +0100

    Added fetch method to hit API endpoint, and state hooks

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+import { useState, useEffect } from "react";
 import "./App.scss";
 import Header from "./components/Header";
 
 const App = () => {
+  const [actors, setActors] = useState([]);
+  const [fetchDone, setFetchDone] = useState([false]);
+
+  const loadActors = async () => {
+    try {
+      const res = await fetch("https://www.breakingbadapi.com/api/characters");
+      const actors = await res.json();
+      setActors(actors);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    loadActors();
+  }, []);
+
   return (
     <div className="container">
       <Header />
