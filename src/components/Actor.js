61ecec86a68a1efb9d926836177149468272e319
mwiby <markuswb77@gmail.com>
2020-11-23 11:13:06 +0100

    Added data to cards and styled card image

--- src/components/Actor.js
+++ src/components/Actor.js
@@ -1,5 +1,43 @@
 const Actor = ({ actor }) => {
-  return <div>{actor.name}</div>;
+  console.log(actor);
+  return (
+    <div className="card">
+      <div className="card-inner">
+        <div className="card-front">
+          <img src={actor.img} alt="actor portrait" />
+        </div>
+        <div className="card-back">
+          <h1>{actor.name}</h1>
+          <ul>
+            <li>
+              <span>Role: </span>
+              {actor.occupation}
+            </li>
+            <li>
+              <span>Status: </span>
+              {actor.status}
+            </li>
+            <li>
+              <span>Nickname: </span>
+              {actor.nickname}
+            </li>
+            <li>
+              <span>Actor Name: </span>
+              {actor.portrayed}
+            </li>
+            <li>
+              <span>Birthday: </span>
+              {actor.birthday}
+            </li>
+            <li>
+              <span>Appearance: </span>
+              {actor.appearance}
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Actor;
